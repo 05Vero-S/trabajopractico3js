caf543398935df193258f503829acc10faf41434
Vero <vdsoria05@gmail.com>
2023-11-11 20:09:35 -0300

    agregue ejercicio 6 del tp3. mediante funcion calcular el perimetro con 2 valores ingresados(operacion dentro de la funcion). muestra en pantalla el resultado

---
+++ js/ejercicio6-tp3.js
@@ -0,0 +1,11 @@
+//Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular su perímetro y mostrarlo por pantalla.La fórmula del perímetro  es p = 2*(a +b)
+
+const ladoA = parseInt(prompt("Ingrese valor del lado A del rectángulo: "));
+const ladoB = parseInt(prompt("Ingrese otro valor del lado B del rectángulo: "));
+
+function perimetro(ladoA,ladoB){
+    let p= 2*(ladoA + ladoB);
+    document.write(`El perímetro del rectangulo es ${p}`);
+}
+
+perimetro(ladoA,ladoB);
