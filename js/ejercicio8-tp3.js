c64a3def95b221318c10fda5cd878cb46df4b4b5
Vero <vdsoria05@gmail.com>
2023-11-12 17:11:19 -0300

    agregue ejercicio 8 del tp3. Funcion para el maximo numero de un arreglo

--- js/ejercicio8-tp3.js
+++ js/ejercicio8-tp3.js
@@ -1,3 +1,15 @@
 //Escribe una función que tome un arreglo de números como argumento y devuelva el número más grande en el arreglo.
 
+const numeros = [2,5,30,25,50,85,1];
+maximo = 0;
 
+function numMax(maximo){
+    for(let i=0; i<numeros.length;i++){
+        if (maximo < numeros[i]){
+            maximo=numeros[i];
+        }
+    }
+    document.write(`El número más grande en el arreglo es: ${maximo}`);
+}
+
+numMax(maximo);
